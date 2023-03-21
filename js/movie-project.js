1876d4197c8aa3adffe9a34dce30d7b50e0e1934
RileyWood9984 <rjw9984@gmail.com>
2023-03-21 11:58:17 -0500

    smoothed out some issues set up some skeleton

--- js/movie-project.js
+++ js/movie-project.js
@@ -1,10 +1,28 @@
-var body = document.querySelector("#header")
-var list;
+let moviesList;
+let customTitle = document.querySelector("#title").innerHTML
+let customRating = document.querySelector("#rating").innerHTML
+let customGenre = document.querySelector("#genre").innerHTML
+let customDirector = document.querySelector("#director").innerHTML
+// console.log(body.innerHTML);
 fetch("https://pinto-goldenrod-lettuce.glitch.me/movies").then(response =>
 {
-    response.json()
+    return response.json()
+}).then(function(data){
+    console.log(data);
+    moviesList=data
+    console.log(moviesList);
 
-}).then(data => {
-    body = for(function(id) {
-        return (`<p>${id.movies.id}</p>`)
-    })})
+    function renderMovie(movie){
+        let html= `<p>${movie.title}</p>`
+        return html;
+    }
+
+    function renderMovies(movies){
+        let html = '';
+        for (let i = 0; i < movies.length; i++) {
+            html += renderMovie(movies[i]);
+        }
+        return html;
+    }
+    document.querySelector("#body").innerHTML= renderMovies(moviesList)
+})
