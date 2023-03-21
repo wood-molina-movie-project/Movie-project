9a17e93a4718602fa398da1f09851fa71d635984
RileyWood9984 <rjw9984@gmail.com>
2023-03-21 14:42:24 -0500

    construction functions working

--- js/movie-project.js
+++ js/movie-project.js
@@ -4,25 +4,34 @@ let customRating = document.querySelector("#rating")
 let customGenre = document.querySelector("#genre")
 let customDirector = document.querySelector("#director")
 // console.log(body.innerHTML);
-fetch("https://pinto-goldenrod-lettuce.glitch.me/movies").then(response =>
-{
+fetch("https://pinto-goldenrod-lettuce.glitch.me/movies").then(response => {
     return response.json()
-}).then(function(data){
+}).then(function (data) {
     console.log(data);
-    moviesList=data
+    moviesList = data
     console.log(moviesList);
 
-    function renderMovie(movie){
-        let html = `<p>${movie.title}</p>`
+    function renderMovie(movie) {
+        let html = `<section class="secOne">`
+        html += `<div class="delete"><button>X</button></div>`
+        html += `<div class="container-sm thisTwo">`
+        html += `<a href="https://placeholder.com"><img src="https://via.placeholder.com/200"></a>`
+        html += `<div class="description">`
+        html += `<h4 class="title">${movie.title}</h4>`
+        html += `<p>${movie.comment}</p></div>`
+
+        html += `</div>`
+        html+=`<div class="holder"><p class="rating">${movie.rating}</p></div></section>`
         return html;
     }
 
-    function renderMovies(movies){
+    function renderMovies(movies) {
         let html = '';
         for (let i = 0; i < movies.length; i++) {
             html += renderMovie(movies[i]);
         }
         return html;
     }
-    document.querySelector("#body").innerHTML= renderMovies(moviesList)
+
+    document.querySelector("#body").innerHTML = renderMovies(moviesList)
 })
