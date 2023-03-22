5a5f9400c6920dd91686b35cea12f3b728a92778
RileyWood9984 <rjw9984@gmail.com>
2023-03-22 11:12:13 -0500

    can add new movie to page

--- js/movie-project.js
+++ js/movie-project.js
@@ -1,15 +1,10 @@
 let temporary = 0
-let imagineId = [ ]
+let imagineId = []
 let moviesList;
-let customTitle = document.querySelector("#title")
-let customRating = document.querySelector("#rating")
-let customGenre = document.querySelector("#genre")
-let customDirector = document.querySelector("#director")
-// console.log(body.innerHTML);
+
 fetch("https://pinto-goldenrod-lettuce.glitch.me/movies").then(response => {
     return response.json()
 }).then(function (data) {
-    console.log(data);
     moviesList = data
     console.log(moviesList);
 
@@ -23,7 +18,7 @@ fetch("https://pinto-goldenrod-lettuce.glitch.me/movies").then(response => {
         html += `<p>${movie.comment}</p></div>`
 
         html += `</div>`
-        html+=`<div class="holder"><p class="rating">${movie.rating}</p></div></section>`
+        html += `<div class="holder"><p class="rating">${movie.rating}</p></div></section>`
         return html;
     }
 
@@ -35,6 +30,60 @@ fetch("https://pinto-goldenrod-lettuce.glitch.me/movies").then(response => {
             html += renderMovie(movies[i]);
         }
         return html;
+    }
+
+    function renderNewMovie(movie) {
+        let html = `<section class="secOne">`
+        html += `<div class="delete"><button>X</button></div>`
+        html += `<div class="container-sm thisTwo">`
+        html += `<a id="${temporary}" href="https://placeholder.com"><img src="https://via.placeholder.com/200"></a>`
+        html += `<div class="description">`
+        html += `<h4 class="title">${movie.title}</h4>`
+        html += `<p>${movie.comment}</p></div>`
+
+        html += `</div>`
+        html += `<div class="holder"><p class="rating">${movie.rating}</p></div></section>`
+        return html;
+    }
+
+    function newMovie(e) {
+        e.preventDefault();
+        let title = document.querySelector('#title').value.toString()
+        let comment = document.querySelector('#comment').value.toString()
+        let director = document.querySelector('#director').value.toString()
+        let rating = document.querySelector('#rating').value.toString()
+        let length = moviesList.length
+        let n = 0
+        let lowerMovie = title.toLowerCase()
+        for (let i = 0; i < length; i++) {
+            let movie = moviesList[i].title.toLowerCase()
+            if (movie === lowerMovie) {
+                n -= 1
+                break;
+            } else if (movie !== lowerMovie) {
+                n += 1
+                if (n !== length) {
+                    continue;
+                } else if (n === length) {
+                    tempMovie = {
+                        title,
+                        director,
+                        comment,
+                        rating
+                    }
+                    moviesList.push(tempMovie)
+                    console.log(moviesList)
+                    document.querySelector("#body").innerHTML += renderNewMovie(tempMovie)
+                }
+            } else {
+                break;
+            }
+        }
+    }
+
+    function deleteMovie(e){
+        e.preventDefault()
+
     }
 
     document.querySelector("#body").innerHTML = renderMovies(moviesList)
@@ -51,7 +100,8 @@ fetch("https://pinto-goldenrod-lettuce.glitch.me/movies").then(response => {
     document.querySelector(`#img-10`).innerHTML = `<img src="img%20/sb.jpeg">`
     document.querySelector(`#img-11`).innerHTML = `<img src="img%20/spr.jpg">`
     document.querySelector(`#img-12`).innerHTML = `<img src="img%20/tho.jpg">`
-
-
-    console.log(imagineId);
+    let addBtn = document.querySelector(`#add`)
+    addBtn.addEventListener('click', newMovie)
+    let deleteBtn = document.querySelector(`.delete`)
+    deleteBtn.addEventListener('click',deleteMovie)
 })
