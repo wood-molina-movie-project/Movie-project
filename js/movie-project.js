31da084a2d082f6c992a0997a37301f53d3fdd91
Oscar Molina <oscar.molina3328@gmail.com>
2023-03-22 10:11:16 -0500

    pictures added

--- js/movie-project.js
+++ js/movie-project.js
@@ -38,8 +38,20 @@ fetch("https://pinto-goldenrod-lettuce.glitch.me/movies").then(response => {
     }
 
     document.querySelector("#body").innerHTML = renderMovies(moviesList)
-    document.querySelector(`#img-0`).innerHTML = `<img src="img%20/smile.jpeg">`
-    document.querySelector(`#img-1`).innerHTML = `<img src="img%20/sb.jpeg">`
-    document.querySelector(`#img-2`).innerHTML = `<img src="img%20/re.jpeg">`
+    document.querySelector(`#img-0`).innerHTML = `<img src="img%20/dip.jpeg.png">`
+    document.querySelector(`#img-1`).innerHTML = `<img src="img%20/tcf.jpeg.png">`
+    document.querySelector(`#img-2`).innerHTML = `<img src="img%20/tqm.jpeg">`
+    document.querySelector(`#img-3`).innerHTML = `<img src="img%20/ones.jpeg">`
+    document.querySelector(`#img-4`).innerHTML = `<img src="img%20/12am.jpeg">`
+    document.querySelector(`#img-5`).innerHTML = `<img src="img%20/sl.jpg">`
+    document.querySelector(`#img-6`).innerHTML = `<img src="img%20/ducks.jpeg">`
+    document.querySelector(`#img-7`).innerHTML = `<img src="img%20/pf.jpg">`
+    document.querySelector(`#img-8`).innerHTML = `<img src="img%20/smile.jpeg">`
+    document.querySelector(`#img-9`).innerHTML = `<img src="img%20/re.jpeg">`
+    document.querySelector(`#img-10`).innerHTML = `<img src="img%20/sb.jpeg">`
+    document.querySelector(`#img-11`).innerHTML = `<img src="img%20/spr.jpg">`
+    document.querySelector(`#img-12`).innerHTML = `<img src="img%20/tho.jpg">`
+
+
     console.log(imagineId);
 })
