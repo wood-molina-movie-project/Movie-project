2b24f7a44b8c46726827e6af751e98063566b29f
RileyWood9984 <rjw9984@gmail.com>
2023-03-22 12:31:56 -0500

    can add new movie to page

--- js/movie-project.js
+++ js/movie-project.js
@@ -1,6 +1,10 @@
+let id;
 let temporary = 0
+let temporary2 = 0
 let imagineId = []
 let moviesList;
+let deleteBtn=[]
+
 
 fetch("https://pinto-goldenrod-lettuce.glitch.me/movies").then(response => {
     return response.json()
@@ -9,8 +13,9 @@ fetch("https://pinto-goldenrod-lettuce.glitch.me/movies").then(response => {
     console.log(moviesList);
 
     function renderMovie(movie) {
+
         let html = `<section class="secOne">`
-        html += `<div class="delete"><button>X</button></div>`
+        html += `<div class="delete"><button id="delete-${id}">X</button></div>`
         html += `<div class="container-sm thisTwo">`
         html += `<a id="${temporary}" href="https://placeholder.com"><img src="https://via.placeholder.com/200"></a>`
         html += `<div class="description">`
@@ -19,12 +24,14 @@ fetch("https://pinto-goldenrod-lettuce.glitch.me/movies").then(response => {
 
         html += `</div>`
         html += `<div class="holder"><p class="rating">${movie.rating}</p></div></section>`
+        deleteBtn += document.querySelector(`#delete-${id}`)
         return html;
     }
 
     function renderMovies(movies) {
         let html = '';
         for (let i = 0; i < movies.length; i++) {
+            id = i
             imagineId[i] = `img-${i}`
             temporary = imagineId[i]
             html += renderMovie(movies[i]);
@@ -34,15 +41,15 @@ fetch("https://pinto-goldenrod-lettuce.glitch.me/movies").then(response => {
 
     function renderNewMovie(movie) {
         let html = `<section class="secOne">`
-        html += `<div class="delete"><button>X</button></div>`
+        html += `<div class="delete"><button id="delete-${++id}">X</button></div>`
         html += `<div class="container-sm thisTwo">`
         html += `<a id="${temporary}" href="https://placeholder.com"><img src="https://via.placeholder.com/200"></a>`
         html += `<div class="description">`
         html += `<h4 class="title">${movie.title}</h4>`
         html += `<p>${movie.comment}</p></div>`
-
         html += `</div>`
         html += `<div class="holder"><p class="rating">${movie.rating}</p></div></section>`
+        deleteBtn += document.querySelector(`#delete-${id}`)
         return html;
     }
 
@@ -81,27 +88,38 @@ fetch("https://pinto-goldenrod-lettuce.glitch.me/movies").then(response => {
         }
     }
 
-    function deleteMovie(e){
-        e.preventDefault()
+    function deleteMovie(event) {
+        event.preventDefault()
+        console.log('i got this far')
+        if (event.target.id === 'delete-1') {
+            console.log('it is doing somthing')
+            fetch(`https://pinto-goldenrod-lettuce.glitch.me/movies`, {
+                method: "DELETE",
+                headers: {
+                    "content-type": "application/json",
+                    accept: "application/json"
+                }
+            })
 
+        }
     }
 
-    document.querySelector("#body").innerHTML = renderMovies(moviesList)
-    document.querySelector(`#img-0`).innerHTML = `<img src="img%20/dip.jpeg.png">`
-    document.querySelector(`#img-1`).innerHTML = `<img src="img%20/tcf.jpeg.png">`
-    document.querySelector(`#img-2`).innerHTML = `<img src="img%20/tqm.jpeg">`
-    document.querySelector(`#img-3`).innerHTML = `<img src="img%20/ones.jpeg">`
-    document.querySelector(`#img-4`).innerHTML = `<img src="img%20/12am.jpeg">`
-    document.querySelector(`#img-5`).innerHTML = `<img src="img%20/sl.jpg">`
-    document.querySelector(`#img-6`).innerHTML = `<img src="img%20/ducks.jpeg">`
-    document.querySelector(`#img-7`).innerHTML = `<img src="img%20/pf.jpg">`
-    document.querySelector(`#img-8`).innerHTML = `<img src="img%20/smile.jpeg">`
-    document.querySelector(`#img-9`).innerHTML = `<img src="img%20/re.jpeg">`
-    document.querySelector(`#img-10`).innerHTML = `<img src="img%20/sb.jpeg">`
-    document.querySelector(`#img-11`).innerHTML = `<img src="img%20/spr.jpg">`
-    document.querySelector(`#img-12`).innerHTML = `<img src="img%20/tho.jpg">`
-    let addBtn = document.querySelector(`#add`)
-    addBtn.addEventListener('click', newMovie)
-    let deleteBtn = document.querySelector(`.delete`)
-    deleteBtn.addEventListener('click',deleteMovie)
+document.querySelector("#body").innerHTML = renderMovies(moviesList)
+document.querySelector(`#img-0`).innerHTML = `<img src="img%20/dip.jpeg.png">`
+document.querySelector(`#img-1`).innerHTML = `<img src="img%20/tcf.jpeg.png">`
+document.querySelector(`#img-2`).innerHTML = `<img src="img%20/tqm.jpeg">`
+document.querySelector(`#img-3`).innerHTML = `<img src="img%20/ones.jpeg">`
+document.querySelector(`#img-4`).innerHTML = `<img src="img%20/12am.jpeg">`
+document.querySelector(`#img-5`).innerHTML = `<img src="img%20/sl.jpg">`
+document.querySelector(`#img-6`).innerHTML = `<img src="img%20/ducks.jpeg">`
+document.querySelector(`#img-7`).innerHTML = `<img src="img%20/pf.jpg">`
+document.querySelector(`#img-8`).innerHTML = `<img src="img%20/smile.jpeg">`
+document.querySelector(`#img-9`).innerHTML = `<img src="img%20/re.jpeg">`
+document.querySelector(`#img-10`).innerHTML = `<img src="img%20/sb.jpeg">`
+document.querySelector(`#img-11`).innerHTML = `<img src="img%20/spr.jpg">`
+document.querySelector(`#img-12`).innerHTML = `<img src="img%20/tho.jpg">`
+let addBtn = document.querySelector(`#add`)
+addBtn.addEventListener('click', newMovie)
+    deleteBtn.forEach()
+deleteBtn.addEventListener('click', deleteMovie)
 })
